c8a3c74c7820c0ed53b0f0d5bd4e3d6bb722f283
Kevin <k.cardenas1795@gmail.com>
2024-01-18 21:52:41 -0500

    creates myfile.md

--- test/indexTest.js
+++ test/indexTest.js
@@ -79,3 +79,40 @@ describe('index.js', function () {
     });
   });
 });
+
+
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(name) {
+  const newCatsArray = [...cats, name];
+  return newCatsArray;
+}
+
+function prependCat(name) {
+  const newCatsArray = [name, ...cats];
+  return newCatsArray;
+}
+
+function removeLastCat() {
+  const newCatsArray = cats.slice(0, -1);
+  return newCatsArray;
+}
+
+function removeFirstCat() {
+  const newCatsArray = cats.slice(1);
+  return newCatsArray;
+}
